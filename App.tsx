8130bc563ae62419eda5d5baafecd2ea41666e0a
Taehoon Yoon <yooontehoon@naver.com>
2020-12-30 22:42:04 +0900

    3. View, Text and Style

--- App.tsx
+++ App.tsx
@@ -3,19 +3,37 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.boldText}>Hello, World!</Text>
+            </View>
+            <View style={styles.body}>
+                <Text style={styles.boldText}>Lorem ipsum dolor sit amet.</Text>
+                <Text>Lorem ipsum dolor sit amet.</Text>
+                <Text>Lorem ipsum dolor sit amet.</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        backgroundColor:'pink',
+        padding:20,
+    },
+    boldText: {
+        fontWeight:'bold',
+    },
+    body:{
+        backgroundColor:'yellow',
+        padding:20,
+        // fontWeight:'bold'
+    },
 });
