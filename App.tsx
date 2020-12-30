4a0574ecda1f299b8df1a64570f7d575d5c70eb4
Taehoon Yoon <yooontehoon@naver.com>
2020-12-30 23:21:39 +0900

    4. State hook (function에서 state사용)

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+    const [name, setName] = useState('Taehun');
+    const [person, setPerson] = useState({ name: 'Mario', age: 40 });
+
+    const clickHandler = () => {
+        setName('hoho');
+        setPerson({ name: 'Luige', age: 35 });
+    }
+    
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.boldText}>Hello, World!</Text>
-            </View>
-            <View style={styles.body}>
-                <Text style={styles.boldText}>Lorem ipsum dolor sit amet.</Text>
-                <Text>Lorem ipsum dolor sit amet.</Text>
-                <Text>Lorem ipsum dolor sit amet.</Text>
+            <Text>My name is {name}</Text>
+            <Text>His name is {person.name} and his age is {person.age}</Text>
+            <View style={styles.buttonContainer}>
+                <Button title="update state" onPress={clickHandler}/>
             </View>
         </View>
     );
@@ -24,16 +29,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    header: {
-        backgroundColor:'pink',
-        padding:20,
-    },
-    boldText: {
-        fontWeight:'bold',
-    },
-    body:{
-        backgroundColor:'yellow',
-        padding:20,
-        // fontWeight:'bold'
+    buttonContainer:{
+        marginTop:20,
     },
 });
