56f77332164ee2549d4e74fcaa9b91b70e8e1530
Taehoon Yoon <yooontehoon@naver.com>
2021-01-01 21:33:05 +0900

    9. Todo App #1 (Header 구현)

--- App.tsx
+++ App.tsx
@@ -1,55 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Header from './assets/scripts/Header';
 
 export default function App() {
-    // const [name, setName] = useState('Taehun');
-    // const [age, setAge] = useState('30');
-    const [people, setPeople] = useState([
-        { name: 'Taehun',id: '1' },
-        { name: 'Mario', id: '2' },
-        { name: 'Luigi', id: '3' },
-        { name: 'Yoshi', id: '4' },
-        { name: 'Peach', id: '5' },
-        { name: 'Toad',  id: '6' },
-        { name: 'Bowse', id: '7' },
-        { name: 'Bowse', id: '8' },
-        { name: 'Bowse', id: '9' },
-        { name: 'Bowse', id: '10' },
-    ]);
 
-    // const clickHandler = () => {
-    //     setName('hoho');
-    //     // setPerson({ name: 'Luige', age: 35 });
-    // }
-    const pressHandler = (id:string) => {
-        console.log(id);
-        setPeople((prevPeople)=> {
-            return prevPeople.filter(person => person.id != id); //배열의 아이템 각각에 필터안의 함수의 결과가 true인 것만 남긴다.
-        })
-    }
+    const [todos, setTodos] = useState([
+        { key: '1', text: 'buy coffee' },
+        { key: '2', text: 'create an app' },
+        { key: '3', text: 'play on the switch' },
+    ]);
     
     return (
         <View style={styles.container}>
-            <FlatList
-                numColumns={2}
-                keyExtractor={(item) => item.id}
-                data={people}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={()=> pressHandler(item.id)}>
-                        <Text style={styles.item}>{item.name}</Text>
-                    </TouchableOpacity>
-                )}
-            />
-            {/* <ScrollView>
-                {people.map((item)=>{ //map은 배열의 아이템각각에 맵안의 함수를 수행한 결과들을 다시 배열에 담아 반환.
-                    return (
-                        <View key={item.key}>
-                            <Text style={styles.item}>{item.name}</Text>
-                        </View>
-                    )
-                })}
-            </ScrollView> */}
+            <Header />
+            <View style={styles.content}>
+                {/* to form */}
+                <View style={styles.list}>
+                    <FlatList 
+                        data={todos}
+                        renderItem={({item})=>(
+                            <Text>{item.text}</Text>
+                        )}
+                    />
+                </View>
+            </View>
         </View>
     );
 }
@@ -58,27 +33,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-        paddingTop: 40,
-        paddingHorizontal: 20,
     },
-    buttonContainer:{
+    content: {
+        padding:40,
+    },
+    list: {
         marginTop:20,
-    },
-    input: {
-        borderWidth:1,
-        borderColor:'#777',
-        padding:8,
-        margin:10,
-        width:200,
-    },
-    item: {
-        marginTop:24,
-        padding:30,
-        backgroundColor:'pink',
-        fontSize:24,
-        marginHorizontal:10,
-        
     },
 });
