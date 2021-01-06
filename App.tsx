28b52a7be81c0c305c4c8a078ff0101014fd2f77
Taehoon Yoon <yooontehoon@naver.com>
2021-01-06 23:41:34 +0900

    async await 추가로 해결. 다른 로딩 방식도 있음.

--- App.tsx
+++ App.tsx
@@ -3,30 +3,30 @@ import * as Font from 'expo-font';
 import Home from './assets/review/screens/Home';
 import AppLoading from 'expo-app-loading';
 
-// const getFonts = () => Font.loadAsync({
-//     'nunito-regular': require('./assets/review/fonts/Nunito/Nunito-Regular.ttf'),
-//     'nunito-bold': require('./assets/review/fonts/Nunito/Nunito-Bold.ttf'),
-// });
+const getFonts = async () => await Font.loadAsync({
+    'nunito-regular': require('./assets/review/fonts/Nunito/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/review/fonts/Nunito/Nunito-Bold.ttf'),
+});
 
 export default function App() {
-    // const [fontsLoaded, setFontsLoaded] = useState(false);
-    const [fontsLoaded] = Font.useFonts({
-        'nunito-regular': require('./assets/review/fonts/Nunito/Nunito-Regular.ttf'),
-        'nunito-bold': require('./assets/review/fonts/Nunito/Nunito-Bold.ttf'),
-    });
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+    // const [fontsLoaded] = Font.useFonts({
+    //     'nunito-regular': require('./assets/review/fonts/Nunito/Nunito-Regular.ttf'),
+    //     'nunito-bold': require('./assets/review/fonts/Nunito/Nunito-Bold.ttf'),
+    // });
     console.log("A");
     if (!fontsLoaded) {
-        console.log("2");
+        console.log("B");
         return (
             <AppLoading 
-                // startAsync={getFonts}
-                // onFinish={()=>setFontsLoaded(true)}
-                // onError={(error)=>{}}
+                startAsync={getFonts}
+                onFinish={()=>setFontsLoaded(true)}
+                onError={(error)=>{}}
             />
         )
     }
     else {
-        console.log("3");
+        console.log("C");
         return (
             <Home />
         )
