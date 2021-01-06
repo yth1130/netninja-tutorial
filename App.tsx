8379aa1f45740a13a534dd6177357b6e1e6d4d60
Taehoon Yoon <yooontehoon@naver.com>
2021-01-06 23:38:12 +0900

    17. Review App #2 (Custom Fonts. async로 안되는 문제?)

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
 import Home from './assets/review/screens/Home';
+import AppLoading from 'expo-app-loading';
+
+// const getFonts = () => Font.loadAsync({
+//     'nunito-regular': require('./assets/review/fonts/Nunito/Nunito-Regular.ttf'),
+//     'nunito-bold': require('./assets/review/fonts/Nunito/Nunito-Bold.ttf'),
+// });
 
 export default function App() {
-    return (
-        <Home />
-    );
+    // const [fontsLoaded, setFontsLoaded] = useState(false);
+    const [fontsLoaded] = Font.useFonts({
+        'nunito-regular': require('./assets/review/fonts/Nunito/Nunito-Regular.ttf'),
+        'nunito-bold': require('./assets/review/fonts/Nunito/Nunito-Bold.ttf'),
+    });
+    console.log("A");
+    if (!fontsLoaded) {
+        console.log("2");
+        return (
+            <AppLoading 
+                // startAsync={getFonts}
+                // onFinish={()=>setFontsLoaded(true)}
+                // onError={(error)=>{}}
+            />
+        )
+    }
+    else {
+        console.log("3");
+        return (
+            <Home />
+        )
+    }
 }
