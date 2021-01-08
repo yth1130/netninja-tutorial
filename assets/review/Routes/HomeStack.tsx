4e8df12d0ef9a0e8480e294193777e94bc560b5e
Taehoon Yoon <yooontehoon@naver.com>
2021-01-08 22:40:40 +0900

    23. Review App #8 (Navigation Options)

--- assets/review/Routes/HomeStack.tsx
+++ assets/review/Routes/HomeStack.tsx
@@ -7,13 +7,26 @@ import ReviewDetails from '../screens/ReviewDetails'
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'GameZone',
+            // headerStyle: { backgroundColor: '#bbb' }
+        }
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+            // headerStyle: { backgroundColor: '#bbb' }
+        }
     },
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#bbb', height:200 }
+    }
+});
 
 export default createAppContainer(HomeStack);
