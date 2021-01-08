e0878145da6e413ca25290d92d1dd37a373ff0d6
Taehoon Yoon <yooontehoon@naver.com>
2021-01-08 20:05:46 +0900

    21. Review App #6 (페이지 이동)

--- assets/review/screens/Home.tsx
+++ assets/review/screens/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { globalStyles } from '../GlobalStyles';
 
-export default function Home() {
+export default function Home({ navigation }: any) {
+    
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+        // navigation.push('ReviewDetails');
+    }
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title='go to review details' onPress={pressHandler}/>
         </View>
     )
 }
--- assets/review/screens/ReviewDetails.tsx
+++ assets/review/screens/ReviewDetails.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { globalStyles } from '../GlobalStyles';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }: any) {
+    const pressHandler = () => {
+        navigation.goBack();
+        // navigation.pop();
+    }
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            <Button title='back to home screen' onPress={pressHandler}/>
         </View>
     )
 }
