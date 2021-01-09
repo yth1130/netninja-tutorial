f6c07a5daf415994152e82dca6bf9dd82f83661a
Taehoon Yoon <yooontehoon@naver.com>
2021-01-09 19:24:16 +0900

    29. Review App #14 (Modal 홈화면에 추가)

--- assets/review/GlobalStyles.tsx
+++ assets/review/GlobalStyles.tsx
@@ -4,6 +4,8 @@ export const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
+        // backgroundColor: 'yellow',
+        backgroundColor: 'white',
     },
     titleText: {
         fontFamily: 'nunito-bold',
--- assets/review/screens/Home.tsx
+++ assets/review/screens/Home.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Modal } from 'react-native'
 import { globalStyles } from '../GlobalStyles';
 import Card from '../shared/Card';
 
+import { MaterialIcons } from '@expo/vector-icons'
+
 export default function Home({ navigation }: any) {
+    const [modalOpen, setModalOpen] = useState(false);
     const [reviews, setReviews] = useState([
         { key: '1', title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum' },
         { key: '2', title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum' },
@@ -11,6 +14,18 @@ export default function Home({ navigation }: any) {
     ])
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons name='close' size={24} onPress={() => {setModalOpen(false)}}
+                        // style={[styles.modalToggle, styles.modalClose]}
+                        style={{...styles.modalToggle, ...styles.modalClose}}
+                    />
+                    <Text>Hello from the modal :)</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons name='add' size={24} onPress={() => {setModalOpen(true)}} style={styles.modalToggle}/>
+            
             <FlatList
                 data={reviews}
                 renderItem={({item})=>(
@@ -24,3 +39,24 @@ export default function Home({ navigation }: any) {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalContent: {
+        flex: 1,
+    },
+    modalToggle: {
+        marginBottom:10,
+        borderWidth:1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+        // justifyContent: 'center',
+        // backgroundColor: 'black',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+        // backgroundColor: 'black',
+    },
+})
